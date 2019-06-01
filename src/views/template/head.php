6cc8d85afd0fd6c5ddffd18962e5a7506dee0187
mike <mmourik@avans.nl>
2019-06-01 13:34:08 +0200

    Adding start for the translation module + Edit on the navigation

--- src/views/template/head.php
+++ src/views/template/head.php
@@ -15,6 +15,13 @@ $userController = new UserController();
     <link href="https://fonts.googleapis.com/css?family=Merriweather|Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- stoart of the translate module -->
+    <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="/src/public/scripts/app_translator.js"></script>
+    <!-- End of the translate module -->
+
     <link rel="stylesheet" href="/src/public/style/style.css">
 </head>
 <body data-spy="scroll" data-target=".follow-navbar" data-offset="50">
@@ -24,11 +31,13 @@ $userController = new UserController();
         <i class="fas fa-bars"></i>
     </button>
     <div id="navList" class="collapse navbar-collapse">
-        <ul class="navbar-nav">
+        <ul class="nav navbar-nav ml-auto w-100 justify-content-start">
             <li class="nav-item"><a href="/index.php" class="nav-link">Home</a></li>
             <li class="nav-item"><a href="#theApp" class="nav-link">De app</a></li>
             <li class="nav-item"><a href="#Company" class="nav-link">Bedrijven</a></li>
             <li class="nav-item"><a href="#aboutUs" class="nav-link">Over ons</a></li>
+        </ul>
+        <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
             <?php if ($userController->isLoggedIn()): ?>
                 <li class="nav-item"><a href="/src/views/sections/user/profile.php" class="nav-link">Profiel</a></li>
                 <li class="nav-item"><a href="/src/views/sections/festivals.php" class="nav-link">Festivals</a></li>
@@ -38,6 +47,15 @@ $userController = new UserController();
                 <li class="nav-item"><a href="/src/views/sections/user/login.php" class="nav-link">Login</a></li>
                 <li class="nav-item"><a href="/src/views/sections/user/register.php" class="nav-link">Registreren</a></li>
             <?php endif; ?>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                NL
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown" id="Translator_language_selector">
+                <a class="dropdown-item" href="#">EN</a>
+                <a class="dropdown-item" href="#">DE</a>
+                </div>
+            </li>
         </ul>
     </div>
 </nav>
