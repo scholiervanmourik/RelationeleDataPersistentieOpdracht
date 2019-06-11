599b0215cefa82b050b4133ba87153746a552ac7
chpsnoij <4xFrietje26%>
2019-06-11 15:44:07 +0200

    Sessie bug oplossen

--- src/views/sections/contact.php
+++ src/views/sections/contact.php
@@ -4,6 +4,76 @@ require_once('../template/head.php');
     <section>
         <article>
             <h3>Contact</h3>
+
+            <?php
+            //define variables and set to empty values
+            $name = $email = $topic = $comment = "";
+
+            //check if form has been submitted
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $name = test_input($_POST["name"]);
+                $email = test_input($_POST["email"]);
+                $topic = test_input($_POST["topic"]);
+                $comment = test_input($_POST["comment"]);
+            }
+            //Submitted True. do validation: strip unnecessary characters like spaces, tabs, newlines, backslahes
+            function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+            //Submitted False: skip validation and display blanc form
+            ?>
+
+
+
+            <!--HTML form with inputs using post superglobal, enduser can send a request for information to xxxxxxx-->
+
+            <!--kunnen we de values automatisch laten vullen met naam vd gebruiker als die bekend is in systeem, wel te overschrijven-->
+
+            <h2>Vraag informatie op bij Support My Day</h2>
+            <form method="post" action="<?php
+            //converts special characters to HTML entities, prevents code injection by means of Cross Site Scripting (XSS)
+            echo htmlspecialchars($_SERVER["PHP_self"]);
+            ?>">
+
+                Naam: <input type="text" name="Name" value= "gebruikersnaam1"> </br>
+                Email: <input type="text" name="Email" value = "gebruikersmailadres1"> </br>
+                Onderwerp:
+                <input type="radio" name="Topic" value="Account"> Mijn gegevens
+                <input type="radio" name="Topic" value="App"> De applicatie
+                <input type="radio" name="Topic" value="Festivals"> Festivals </br>
+                Beschrijving: <textarea name="Comment" rows="10" cols="40"></textarea> </br>
+                <input type="submit">
+            </form>
+
+            <?php
+            echo "<h2>Your Input:</h2>";
+            echo $name . "</br>";
+            ?>
+
+            <?php
+            //define variables and set to empty values
+            $name = $email = $topic = $comment = "";
+
+            //check if form has been submitted
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $name = test_input($_POST["name"]);
+                $email = test_input($_POST["email"]);
+                $topic = test_input($_POST["topic"]);
+                $comment = test_input($_POST["comment"]);
+            }
+            //Submitted True. do validation: strip unnecessary characters like spaces, tabs, newlines, backslahes
+            function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+            //Submitted False: skip validation and display blanc form
+            ?>
+
         </article>
     </section>
 <?php require_once('../template/bottom.php') ?>
