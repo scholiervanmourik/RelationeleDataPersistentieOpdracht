9144dced809cc73d4890eedd550ee07d1f147096
chpsnoij <c.snoijs@ziggo.nl>
2019-06-13 23:54:23 +0200

    created request-for-information form for enduser

--- src/views/sections/contact.php
+++ src/views/sections/contact.php
@@ -5,56 +5,123 @@ require_once('../template/head.php');
         <article>
             <h3>Contact</h3>
 
+            <style>
+                .error {color: #C2E812;}   <!--verbeteren:kleur uit stylesheet halen-->
+            </style>
+
+            <!--HTML form with inputs using POST superglobal, enduser can send a request for information to SMD email-->
             <?php
-            //define variables and set to empty values
+            // define variables and set to empty values
+            $nameErr = $emailErr = $topicErr = $commentErr = "";
             $name = $email = $topic = $comment = "";
 
-            //check if form has been submitted
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $name = test_input($_POST["name"]);
-                $email = test_input($_POST["email"]);
-                $topic = test_input($_POST["topic"]);
-                $comment = test_input($_POST["comment"]);
+                if (empty($_POST["name"])) {
+                    $nameErr = "Naam is een verplicht veld";   //alert for user when name hasn't been entered
+                } else {
+                    $name = test_input($_POST["name"]);
+
+                    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+                        $nameErr = "Gebruik alleen letters en spaties";    //check if name only contains letters and whitespace
+                    }
+                }
+
+                if (empty($_POST["email"])) {
+                    $emailErr = "E-mail is een verplicht veld";
+                } else {
+                    $email = test_input($_POST["email"]);
+
+                    // check if e-mail address is well-formed
+                    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        $emailErr = "Gebruik alleen een geldig email adres";
+                    }
+                }
+
+                if (empty($_POST["topic"])) {
+                    $topicErr = "Kies een onderwerp";
+                } else {
+                    $topic = test_input($_POST["topic"]);
+                }
+
+                if (empty($_POST["comment"])) {
+                    $commentErr = "Vul hier je vraag in, dit is een verplicht veld";
+                } else {
+                    $comment = test_input($_POST["comment"]);
+                }
             }
-            //Submitted True. do validation: strip unnecessary characters like spaces, tabs, newlines, backslahes
+
+
             function test_input($data) {
                 $data = trim($data);
-                $data = stripslashes($data);
+                $data = stripslashes($data);     //deze doet ie vlgs mij niet
                 $data = htmlspecialchars($data);
                 return $data;
             }
-            //Submitted False: skip validation and display blanc form
             ?>
-
-
-
-            <!--HTML form with inputs using post superglobal, enduser can send a request for information to xxxxxxx-->
-
-            <!--kunnen we de values automatisch laten vullen met naam vd gebruiker als die bekend is in systeem, wel te overschrijven-->
 
             <h2>Vraag informatie op bij Support My Day</h2>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            <!-- converts special characters to HTML entities, prevents code injection by means of Cross Site Scripting (XSS) -->
-            
+            <p><span class="error">* verplicht veld</span></p>
 
-                Naam: <input type="text" name="Name" value="<?php echo $firstName ?>"> </br>
-                Lastname: <input type="text" name="Name" value="<?php echo $lastName ?>"> </br>
-                Email: <input type="text" name="Email" value="<?php echo $email ?>"> </br>
+            <!-- htmlspecialchars converts special characters to HTML entities, prevents code injection by means of Cross Site Scripting (XSS) -->
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+                <!--echo $name in input value makes that value is restored when one of the other fields is rejected-->
+                Naam: <input type="text" name="name" value="<?php echo $name;?>">
+
+                <span class="error">* <?php echo $nameErr;?></span>
+                <br><br>
+
+                E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+                <span class="error">* <?php echo $emailErr;?></span>
+                <br><br>
 
                 Onderwerp:
-                <input type="radio" name="Topic" value="Account" > Mijn gegevens
-                <input type="radio" name="Topic" value="App"> De applicatie
-                <input type="radio" name="Topic" value="Festivals"> Festivals 
-            </br>
-                Beschrijving: <textarea name="Comment" rows="10" cols="40"></textarea> </br>
-                <input type="submit">
+                <input type="radio" name="topic"
+                    <?php if (isset($gender) && $gender=="account")
+                        echo "checked";?>
+                       value="account">Mijn gegevens
+
+                <input type="radio" name="topic"
+                    <?php if (isset($topic) && $topic=="app")
+                        echo "checked";?>
+                       value="app">De applicatie
+
+                <input type="radio" name="topic"
+                    <?php if (isset($topic) && $topic=="festivals")
+                        echo "checked";?>
+                       value="festivals">Festivals
+
+                <span class="error">* <?php echo $topicErr;?></span>
+                <br><br>
+
+                Vraag: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>  <!--size nog corrigeren-->
+                <br><br>
+
+                <input type="submit" name="submit" value="Verzend">
             </form>
 
+            <!--verbeteren: onderstaand php segment weghalen voor live versie-->
             <?php
             echo "<h2>Your Input:</h2>";
-            echo $name . "</br>";
+            echo $name;
+            echo "<br>";
+            echo $email;
+            echo "<br>";
+            echo $topic;
+            echo "<br>";
+            echo $comment;
             ?>
 
+            <?php
+            //creating the message
+            $msg = "$name \n $email \n $topic \n $comment";
+
+            //wordwrap if lines are over 50 characters long
+            $msg = wordwrap($msg,50);
+
+            //actual sending mail command
+            mail("chpsnoij@avans.nl","Gebruikersvraag",$msg);
+            ?>
         </article>
     </section>
 <?php require_once('../template/bottom.php') ?>
