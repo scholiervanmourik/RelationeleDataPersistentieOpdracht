da4bdb8e83417dfeb2e9d3869b902c7c2c32e455
mike <mmourik@avans.nl>
2019-06-13 20:47:02 +0200

    Login form, style update
    hier word het formulier om in te loggen visueel versterkt

--- src/views/sections/user/login.php
+++ src/views/sections/user/login.php
@@ -5,15 +5,34 @@ require_once('../../template/head.php');
         <article>
             <h3>Login</h3>
             <form class="xhr-form" action="../../../actions/login.php" data-callback="login" method="POST">
-                <div class="form-group">
-                    <input type="text" name="email" placeholder="Email">
+                <div class="form-group">   
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">Email</div>
+                        </div>
+                        <input type="text" placeholder="Email" name="email" class="form-control">
+                    </div>
                 </div>
                 <div class="form-group">
-                    <input type="password" name="password" placeholder="Wachtwoord">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">Email</div>
+                        </div>
+                        <input type="password" name="password" placeholder="Wachtwoord" class="form-control">
+                    </div>
                 </div>
                 <div class="form-group">
-                    <button type="submit">Login</button>
-                    <a href="register.php">Registreren</a>
+                    <button type="submit" style="background:transparent; border:0; outline:none; padding:0;">
+                        <a class="btn">
+                            <div class="btn-text">Login</div>
+                            <div class="button"></div>
+                        </a>
+                    </button>
+
+                    <a href="register.php" class="btn" target="_self">
+                        <div class="btn-text">Registreren</div>
+                        <div class="button"></div>
+                    </a>
                 </div>
             </form>
         </article>
